b6da5f19f7f03c342dcd6d29329756ef472073ab
Metodi-Angelov <sustavbg@gmail.com>
2020-11-28 12:15:41 +0200

    New solution

--- decorators/cache.py
+++ decorators/cache.py
@@ -1,8 +1,23 @@
 def cache(fn):
-    log = {}
+    def wrapper(n):
+        result = fn(n)
+        wrapper.log[n] = result
+        return result
 
+    wrapper.log = {}
+
+    return wrapper
 
 
 @cache
 def fibonacci(n):
-    pass
+    if n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+fibonacci(3)
+print(fibonacci.log)
+
+fibonacci(4)
+print(fibonacci.log)
